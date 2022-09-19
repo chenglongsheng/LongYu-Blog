26ad2a05224e99ac935447afd50895c60eb0a42e
成龙生 <1536463948@qq.com>
2022-09-19 23:42:30 +0800

    系统常量

---
+++ longyu-common/src/main/java/com/longyu/common/constont/SystemConstant.java
@@ -0,0 +1,21 @@
+package com.longyu.common.constont;
+
+public class SystemConstant {
+    /**
+     * 文章草稿
+     */
+    public static final int ARTICLE_STATUS_DRAFT = 1;
+    /**
+     * 文章已发布
+     */
+    public static final int ARTICLE_STATUS_PUBLISHED = 0;
+    /**
+     * 分页-当前页
+     */
+    public static final long PAGE_CURRENT = 1;
+    /**
+     * 分页-每页显示条数
+     */
+    public static final long PAGE_SIZE = 10;
+
+}
