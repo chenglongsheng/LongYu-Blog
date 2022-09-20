60fbff901e2effc196d426f9e71a710899236970
成龙生 <1536463948@qq.com>
2022-09-20 14:18:44 +0800

    自动添加时间

--- longyu-common/src/main/java/com/longyu/common/domain/entity/Article.java
+++ longyu-common/src/main/java/com/longyu/common/domain/entity/Article.java
@@ -1,9 +1,6 @@
 package com.longyu.common.domain.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -77,6 +74,7 @@ public class Article implements Serializable {
     /**
      *
      */
+    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
     /**
@@ -87,6 +85,7 @@ public class Article implements Serializable {
     /**
      *
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
 
     /**
