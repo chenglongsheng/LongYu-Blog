f8a46ad6abfdc4bfd2d5ce5ecec47b446923432a
成龙生 <1536463948@qq.com>
2022-09-20 14:17:53 +0800

    修改配置

--- longyu-blog/src/main/java/com/longyu/blog/LongyuBlogApplication.java
+++ longyu-blog/src/main/java/com/longyu/blog/LongyuBlogApplication.java
@@ -1,11 +1,9 @@
 package com.longyu.blog;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.longyu")
-@MapperScan(basePackages = "com.longyu.common.mapper")
 public class LongyuBlogApplication {
 
     public static void main(String[] args) {
