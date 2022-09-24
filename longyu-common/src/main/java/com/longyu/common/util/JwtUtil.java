bfefb1f7b6613ed25fc75f9e1957e05c0348d627
成龙生 <1536463948@qq.com>
2022-09-24 09:58:05 +0800

    注入容器管理

--- longyu-common/src/main/java/com/longyu/common/util/JwtUtil.java
+++ longyu-common/src/main/java/com/longyu/common/util/JwtUtil.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -14,14 +16,26 @@ import java.util.UUID;
 /**
  * JWT工具类
  */
+@Component
 public class JwtUtil {
 
-    //有效期为60 * 60 *1000  一个小时
-    public static final Long JWT_TTL = 24 * 60 * 60 * 1000L;
-    //设置秘钥明文
-    public static final String JWT_KEY = "sangeng";
+    /**
+     * 过期时间
+     */
+    @Value("${myapp.jwt.expire}")
+    private Long EXPIRE;
+    /**
+     * 秘钥明文
+     */
+    @Value("${myapp.jwt.key}")
+    private String KEY;
+    /**
+     * 签发者
+     */
+    @Value("${myapp.jwt.issuer}")
+    private String ISSUER;
 
-    public static String getUUID() {
+    public String getUUID() {
         return UUID.randomUUID().toString().replaceAll("-", "");
     }
 
@@ -31,7 +45,7 @@ public class JwtUtil {
      * @param subject token中要存放的数据（json格式）
      * @return
      */
-    public static String createJWT(String subject) {
+    public String createJWT(String subject) {
         JwtBuilder builder = getJwtBuilder(subject, null, getUUID());// 设置过期时间
         return builder.compact();
     }
@@ -43,24 +57,24 @@ public class JwtUtil {
      * @param ttlMillis token超时时间
      * @return
      */
-    public static String createJWT(String subject, Long ttlMillis) {
+    public String createJWT(String subject, Long ttlMillis) {
         JwtBuilder builder = getJwtBuilder(subject, ttlMillis, getUUID());// 设置过期时间
         return builder.compact();
     }
 
-    private static JwtBuilder getJwtBuilder(String subject, Long ttlMillis, String uuid) {
+    private JwtBuilder getJwtBuilder(String subject, Long ttlMillis, String uuid) {
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
         SecretKey secretKey = generalKey();
         long nowMillis = System.currentTimeMillis();
         Date now = new Date(nowMillis);
         if (ttlMillis == null) {
-            ttlMillis = JwtUtil.JWT_TTL;
+            ttlMillis = EXPIRE;
         }
         long expMillis = nowMillis + ttlMillis;
         Date expDate = new Date(expMillis);
         return Jwts.builder().setId(uuid)              //唯一的ID
                 .setSubject(subject)   // 主题  可以是JSON数据
-                .setIssuer("sg")     // 签发者
+                .setIssuer(ISSUER)     // 签发者
                 .setIssuedAt(now)      // 签发时间
                 .signWith(signatureAlgorithm, secretKey) //使用HS256对称加密算法签名, 第二个参数为秘钥
                 .setExpiration(expDate);
@@ -74,7 +88,7 @@ public class JwtUtil {
      * @param ttlMillis
      * @return
      */
-    public static String createJWT(String id, String subject, Long ttlMillis) {
+    public String createJWT(String id, String subject, Long ttlMillis) {
         JwtBuilder builder = getJwtBuilder(subject, ttlMillis, id);// 设置过期时间
         return builder.compact();
     }
@@ -82,12 +96,11 @@ public class JwtUtil {
     /**
      * 生成加密后的秘钥 secretKey
      *
-     * @return
+     * @return 加密后的秘钥
      */
-    public static SecretKey generalKey() {
-        byte[] encodedKey = Base64.getDecoder().decode(JwtUtil.JWT_KEY);
-        SecretKey key = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
-        return key;
+    public SecretKey generalKey() {
+        byte[] encodedKey = Base64.getDecoder().decode(KEY);
+        return new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
     }
 
     /**
@@ -97,7 +110,7 @@ public class JwtUtil {
      * @return
      * @throws Exception
      */
-    public static Claims parseJWT(String jwt) throws Exception {
+    public Claims parseJWT(String jwt) throws Exception {
         SecretKey secretKey = generalKey();
         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwt).getBody();
     }
