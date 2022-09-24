e917edac2736aa2bf78621d7c0d94241cdb4a2b5
成龙生 <1536463948@qq.com>
2022-09-24 09:15:45 +0800

    生成加密密码

--- longyu-blog/src/test/java/com/longyu/blog/LongyuBlogApplicationTests.java
+++ longyu-blog/src/test/java/com/longyu/blog/LongyuBlogApplicationTests.java
@@ -1,13 +1,23 @@
 package com.longyu.blog;
 
+import com.longyu.common.util.JwtUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class LongyuBlogApplicationTests {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     void contextLoads() {
+
+        String encode = passwordEncoder.encode("123");
+        System.out.println(encode);
+
     }
 
 }
